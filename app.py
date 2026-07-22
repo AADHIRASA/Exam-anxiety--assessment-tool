dcd66eb6aff210b656fc1cf5bb1a08fae40d6be4
AADHIRASA <aadhiraaristotle@gmail.com>
2026-07-22 12:50:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import sqlite3
 app=Flask(__name__)
-@app.route("/")
+@app.route("/" , methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    db= sqlite3.connect("anxiety.db")
+    if request.method == "POST":
+        q1 = request.form.get ("q1")
+        q2 = request.form.get ("q2")
+        q3 = request.form.get ("q3")
+        q4 = request.form.get ("q4")
+        q5 = request.form.get ("q5")
+        db.execute("Insert into responses ( q1,q2,q3,q4,q5 ) VALUES (?,?,?,?,?)", (q1,q2,q3,q4,q5) )
+        db.commit()
+    count=db.execute( "SELECT COUNT (*) FROM responses").fetchone ()[0]
+    db.close()
+    return render_template("index.html", user_count=count)
